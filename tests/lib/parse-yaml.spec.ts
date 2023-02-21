1239daaba12e930fcb66d35eb0fef8f395a8162d
zbitname <i@zbit.name>
2023-02-21 23:57:08 +0300

    tests: refactor

--- tests/lib/parse-yaml.spec.ts
+++ tests/lib/parse-yaml.spec.ts
@@ -21,116 +21,37 @@ describe('YAML parse', () => {
             type: 'object',
             value: [],
             children: [
-                {
-                    type: 'string',
-                    prop: 'prop1',
-                    value: 'some string',
-                },
-                {
-                    type: 'number',
-                    prop: 'prop2',
-                    value: 123,
-                },
-                {
-                    type: 'number',
-                    prop: 'prop3',
-                    value: 123.456,
-                },
-                {
-                    type: 'boolean',
-                    prop: 'prop4',
-                    value: true,
-                },
-                {
-                    type: 'null',
-                    prop: 'prop5',
-                    value: null,
-                },
-                {
-                    type: 'array',
-                    value: [],
-                    children: [],
-                },
-                {
-                    type: 'object',
-                    value: [],
-                    children: [],
-                },
+                { type: 'string', prop: 'prop1', value: 'some string' },
+                { type: 'number', prop: 'prop2', value: 123 },
+                { type: 'number', prop: 'prop3', value: 123.456 },
+                { type: 'boolean', prop: 'prop4', value: true },
+                { type: 'null', prop: 'prop5', value: null },
+                { type: 'array', value: [], children: [] },
+                { type: 'object', value: [], children: [] },
                 {
                     type: 'array',
                     value: [],
                     children: [
-                        {
-                            type: 'string',
-                            value: 'val1',
-                        },
-                        {
-                            type: 'number',
-                            value: 234,
-                        },
-                        {
-                            type: 'number',
-                            value: 234.345,
-                        },
-                        {
-                            type: 'boolean',
-                            value: true,
-                        },
-                        {
-                            type: 'null',
-                            value: null,
-                        },
-                        {
-                            type: 'array',
-                            value: [],
-                            children: [],
-                        },
-                        {
-                            type: 'object',
-                            value: [],
-                            children: [],
-                        },
+                        { type: 'string', value: 'val1' },
+                        { type: 'number', value: 234 },
+                        { type: 'number', value: 234.345 },
+                        { type: 'boolean', value: true },
+                        { type: 'null', value: null },
+                        { type: 'array', value: [], children: [] },
+                        { type: 'object', value: [], children: [] },
                     ],
                 },
                 {
                     type: 'object',
                     value: [],
                     children: [
-                        {
-                            type: 'string',
-                            prop: 'subprop1',
-                            value: 'val1',
-                        },
-                        {
-                            type: 'number',
-                            prop: 'subprop2',
-                            value: 234,
-                        },
-                        {
-                            type: 'number',
-                            prop: 'subprop3',
-                            value: 234.345,
-                        },
-                        {
-                            type: 'boolean',
-                            prop: 'subprop4',
-                            value: true,
-                        },
-                        {
-                            type: 'null',
-                            prop: 'subprop5',
-                            value: null,
-                        },
-                        {
-                            type: 'array',
-                            value: [],
-                            children: [],
-                        },
-                        {
-                            type: 'object',
-                            value: [],
-                            children: [],
-                        },
+                        { type: 'string', prop: 'subprop1', value: 'val1' },
+                        { type: 'number', prop: 'subprop2', value: 234 },
+                        { type: 'number', prop: 'subprop3', value: 234.345 },
+                        { type: 'boolean', prop: 'subprop4', value: true },
+                        { type: 'null', prop: 'subprop5', value: null },
+                        { type: 'array', value: [], children: [] },
+                        { type: 'object', value: [], children: [] },
                     ],
                 },
             ],
@@ -145,18 +66,9 @@ describe('YAML parse', () => {
             type: 'array',
             value: [],
             children: [
-                {
-                    type: 'string',
-                    value: 'item1',
-                },
-                {
-                    type: 'string',
-                    value: 'item2',
-                },
-                {
-                    type: 'string',
-                    value: 'item3',
-                },
+                { type: 'string', value: 'item1' },
+                { type: 'string', value: 'item2' },
+                { type: 'string', value: 'item3' },
             ],
         });
     });
@@ -164,6 +76,19 @@ describe('YAML parse', () => {
     it('mixed types in root array items', () => {
         const content = readFileSync(`${__dirname}/files/mixed-types-root-array.yaml`).toString();
         const res = parse(content, '');
-        console.log(res[0].getChartItem());
+        const chartItem = res[0].getChartItem();
+        expect(chartItem).to.deep.equals({
+            type: 'array',
+            value: [],
+            children: [
+                { type: 'null', value: null },
+                { type: 'number', value: 123 },
+                { type: 'number', value: 123.456 },
+                { type: 'boolean', value: true },
+                { type: 'string', value: 'some string' },
+                { type: 'array', value: [], children: [] },
+                { type: 'object', value: [], children: [] },
+            ],
+        });
     });
 });
