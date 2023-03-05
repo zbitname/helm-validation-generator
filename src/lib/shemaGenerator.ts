c5f31950bb8ba83ebb60e884ce899482e989b63d
zbitname <i@zbit.name>
2023-03-05 16:04:30 +0300

    chore: intermediate

--- src/lib/shemaGenerator.ts
+++ src/lib/shemaGenerator.ts
@@ -1,7 +1,27 @@
 import {
-  IChartItem,
+  IChartItemWithOperations,
 } from './interfaces';
 
-export const getJSONSchema = (doc: IChartItem): any => {
-  // console.log(JSON.stringify(doc, null, 2));
+export const getJSONSchemaFromFlat = (chartItems: IChartItemWithOperations[]): any => {
+  const flatSchema: any[] = [];
+  const processedTemplates: string[] = [];
+
+  for (let i = 0; i < chartItems.length; i++) {
+    const item = chartItems[i];
+
+    if (!item.path) continue;
+    if (item.operations.skip) continue;
+
+    // if .path is a part of array
+    if (/\[/.test(item.path)) {
+      console.log(item.path, item, chartItems.filter(i => i.pathTemplate === item.path));
+      break;
+
+      continue;
+    }
+
+    processedTemplates.push(item.path);
+  }
+
+  return flatSchema;
 };
