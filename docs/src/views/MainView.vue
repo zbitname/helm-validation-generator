2612295e63f8f762f10f105d8eb87dfeff0afd4a
zbitname <i@zbit.name>
2023-04-09 23:31:33 +0300

    chore(docs): update main page

--- docs/src/views/MainView.vue
+++ docs/src/views/MainView.vue
@@ -6,9 +6,10 @@ export default {}
 </script>
 
 <template>
-  <h1>What you can do?</h1>
+  <h1>Very short description</h1>
   <p>Making JSON Schema wasn't so easy ever. <RouterLink to="/generator">Let's try to make a schema now</RouterLink>.</p>
-  <p>Or read the <RouterLink to="/docs">documentation</RouterLink> for making advanced schema with hint-comments.</p>
+  <h1>What you can do?</h1>
+  <p>Read the <RouterLink to="/docs">documentation</RouterLink> for making advanced schema with hint-comments.</p>
   <p>You can give the <a href="https://github.com/zbitname/helm-validation-generator">repository</a> a star if you found this tool useful enough.</p>
   <p>If you have some problems, you can make an <a href="https://github.com/zbitname/helm-validation-generator/issues">issue</a>.</p>
   <p>You can also make <a href="https://github.com/zbitname/helm-validation-generator/pulls">PR</a> to improve this tool.</p>
