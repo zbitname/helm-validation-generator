5e7785772c05ecb89237a844239fea794e9d5886
zbitname <i@zbit.name>
2023-04-09 19:33:40 +0300

    fix(docs): fix base path

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,10 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+let base = '/' + (process.env.BASE_URL || '');
+
+if (!base.endsWith('/')) {
+  base += '/';
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/' + (process.env.BASE_URL || ''),
+  base,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
